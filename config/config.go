0be38c6484933d283ffdc64d79d86361e12beaf1
Bruce Potter <bp@us.ibm.com>
2017-02-26 08:18:36 -0500

    completed Config object

--- config/config.go
+++ config/config.go
@@ -1,32 +1,130 @@
+// Package config provides the object for reading a mendel input file and accessing the variables in it.
 package config
 
 import (
 	//"fmt"
-	"os"
+	//"os"
 	//"path/filepath"
-	"github.com/naoina/toml"
+	"github.com/naoina/toml" 		// implementation of TOML we are using to read input files
 	"io/ioutil"
 	//"bitbucket.org/geneticentropy/mendel-go/utils"
 	"log"
 )
 
+// Config is the struct that gets filled in by TOML automatically from the input file.
 type Config struct {
-	Name string
-	General struct {
-		Verbose uint
+	Basic struct {
+		Case_id string
+		Mutn_rate float32
+		Frac_fav_mutn float32
+		Reproductive_rate float32
+		Pop_size int
+		Num_generations int
+	}
+	Mutations struct {
+		Fitness_distrib_type int
+		Fraction_neutral float32
+		Genome_size float32
+		High_impact_mutn_fraction float32
+		High_impact_mutn_threshold float32
+		Num_initial_fav_mutn int
+		Max_fav_fitness_gain float32
+		Uniform_fitness_effect_del float32
+		Uniform_fitness_effect_fav float32
+		Fraction_recessive float32
+		Recessive_hetero_expression float32
+		Dominant_hetero_expression float32
+		Multiplicative_weighting float32
+		Synergistic_epistasis bool
+		Se_nonlinked_scaling float32
+		Se_linked_scaling float32
+		Upload_mutations bool
+		Allow_back_mutn bool
+		Polygenic_beneficials bool
+		Polygenic_init string
+		Polygenic_target string
+		Polygenic_effect float32
+	}
+	Selection struct {
+		Fraction_random_death float32
+		Heritability float32
+		Non_scaling_noise float32
+		Fitness_dependent_fertility bool
+		Selection_scheme int
+		Partial_truncation_value float32
+	}
+	Population struct {
+		Recombination_model int
+		Fraction_self_fertilization float32
+		Num_contrasting_alleles int
+		Max_total_fitness_increase int
+		Initial_alleles_pop_frac float32
+		Initial_alleles_amp_factor int
+		Dynamic_linkage bool
+		Haploid_chromosome_number int
+		Num_linkage_subunits int
+		Pop_growth_model int
+		Pop_growth_rate float32
+		Carrying_capacity int
+		Bottleneck_yes bool
+		Bottleneck_generation int
+		Bottleneck_pop_size int
+		Num_bottleneck_generations int
+	}
+	Substructure struct {
+		Is_parallel bool
+		Homogenous_tribes bool
+		Num_indiv_exchanged int
+		Migration_generations int
+		Migration_model int
+		Tribal_competition bool
+		Tribal_fission bool
+		Tc_scaling_factor float32
+		Group_heritability float32
+		Social_bonus_factor float32
+	}
+	Computation struct {
+		Tracking_threshold float64
+		Extinction_threshold float64
+		Max_del_mutn_per_indiv int
+		Max_neu_mutn_per_indiv int
+		Max_fav_mutn_per_indiv int
+		Random_number_seed int
+		Reseed_rng bool
+		Track_neutrals bool
+		Write_dump bool
+		Write_vcf bool
+		Restart_case bool
+		Restart_dump_number int
+		Plot_allele_gens int
+		Verbosity int
+		Data_file_path string
 	}
 }
 
-var Cfg *Config 		// singleton config
+// Cfg is the singleton instance of Config that can be accessed throughout the mendel code.
+// It gets set in ReadFromFile().
+var Cfg *Config
 
+// ReadFromFile reads the specified input file and parses all of the values into the Config struct.
 func (c *Config) ReadFromFile(filename string) error {
-	log.Printf("Reading %v...\n", filename) 	// can not use Verbose here because we have not read the config file yet
-	f, err := os.Open(filename)
-	if err != nil { return err }
-	defer f.Close()
-	buf, err := ioutil.ReadAll(f)
+	log.Printf("Reading %v...\n", filename) 	// can not use verbosity here because we have not read the config file yet
+	//f, err := os.Open(filename)
+	//if err != nil { return err }
+	//defer f.Close()
+	buf, err := ioutil.ReadFile(filename)
 	if err != nil { return err }
 	if err := toml.Unmarshal(buf, c); err != nil { return err }
 	Cfg = c 		// set the singleton config
 	return nil
 }
+
+/* toml.Marshal writes floats in a long exponention form not convenient
+func (c *Config) WriteToFile(filename string) error {
+	log.Printf("Writing %v...\n", filename)
+	buf, err := toml.Marshal(*c)
+	if err != nil { return err }
+	if err := ioutil.WriteFile(filename, buf, 0644); err != nil { return err }
+	return nil
+}
+*/
--- utils/utils.go
+++ utils/utils.go
@@ -7,8 +7,18 @@ import (
 	//"path/filepath"
 	//"io/ioutil"
 	"bitbucket.org/geneticentropy/mendel-go/config"
+	"io/ioutil"
 )
 
-func Verbose(level uint, msg string, args ...interface{}) {
-	if level >= config.Cfg.General.Verbose { log.Printf("Verbose "+fmt.Sprint(level)+": "+msg, args...) }
+func Verbose(level int, msg string, args ...interface{}) {
+	if level >= config.Cfg.Computation.Verbosity { log.Printf("Verbose "+fmt.Sprint(level)+": "+msg, args...) }
 }
+
+func CopyFile(fromFile, toFile string) error {
+	log.Printf("Copying %v to %v...\n", fromFile, toFile) 	// can not use verbosity here because we have not read the config file yet
+	buf, err := ioutil.ReadFile(fromFile)
+	if err != nil { return err }
+	if err := ioutil.WriteFile(toFile, buf, 0644); err != nil { return err }
+	return nil
+}
+
